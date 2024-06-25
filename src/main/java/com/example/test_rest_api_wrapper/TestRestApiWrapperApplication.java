a3af36ab09616e404814b190e165e13abe1018f1
IshchenkoDan <ishchenko44@gmail.com>
2024-06-25 18:56:01 +0300

    project skeleton commit

---
+++ src/main/java/com/example/test_rest_api_wrapper/TestRestApiWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_rest_api_wrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestApiWrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRestApiWrapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_rest_api_wrapper/TestRestApiWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_rest_api_wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestApiWrapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
